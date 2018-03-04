48b998a973308d20a79b8ac56efec55be1150895
wonwoo <aoruqjfu@gmail.com>
2018-03-04 20:38:47 +0900

    spring boot 2.0 release update

--- src/main/java/io/spring/demo/issuesdashboard/WebSecurityConfig.java
+++ src/main/java/io/spring/demo/issuesdashboard/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package io.spring.demo.issuesdashboard;
 
-import org.springframework.boot.actuate.autoconfigure.security.EndpointRequest;
-import org.springframework.boot.autoconfigure.security.StaticResourceRequest;
+//import org.springframework.boot.actuate.autoconfigure.security.EndpointRequest;
+//import org.springframework.boot.autoconfigure.security.StaticResourceRequest;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,10 +20,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 		return new InMemoryUserDetailsManager(
-				User.withDefaultPasswordEncoder().username("user").password("password")
+				User.withUsername("user").password("password")
+						.passwordEncoder(passwordEncoder()::encode)
 						.authorities("ROLE_USER").build(),
-				User.withDefaultPasswordEncoder().username("admin").password("admin")
+				User.withUsername("admin").password("admin")
+						.passwordEncoder(passwordEncoder()::encode)
 						.authorities("ROLE_ACTUATOR", "ROLE_ADMIN", "ROLE_USER").build());
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 	@Override
@@ -27,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/admin").hasRole("ADMIN")
 				.requestMatchers(EndpointRequest.to("info", "health")).permitAll()
 				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
-				.requestMatchers(StaticResourceRequest.toCommonLocations()).permitAll()
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
 				.antMatchers("/events/**").hasRole("USER")
 				.antMatchers("/**").permitAll()
 				.and().httpBasic();
